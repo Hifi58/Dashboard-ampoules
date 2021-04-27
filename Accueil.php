3be8955250a5ee8e5fe0f9cbb09f3f00929c4a0b
Hifi58 <gillet.tony@gmail.com>
2021-04-27 17:02:01 +0200

    fin de journée 1

--- Accueil.php
+++ Accueil.php
@@ -1,3 +1,15 @@
+<?php
+try{
+$bdd = new PDO('mysql:host=localhost;dbname=ampoules;charset=utf8', 'root', '');
+}
+catch (Exception $e)
+{
+        die('Erreur : ' . $e->getMessage());
+}
+
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
