e7ac5397e75f55fbd04cc39748d3746f133c7f30
Hifi58 <gillet.tony@gmail.com>
2021-04-30 16:57:53 +0200

    MAJ

--- supprimer.php
+++ supprimer.php
@@ -13,8 +13,7 @@ $id = $_GET['id'];
 $bdd->exec("DELETE FROM historique WHERE id = '$id'");
 
 echo ("<script LANGUAGE='JavaScript'>
-window.alert('Suppression effectué');
-window.location.href='historique.php';
+onclick=\"if(window.confirm('Voulez-vous vraiment supprimer ?')){return true;}else{return false;}\";
 </script>");
 ?>
 
