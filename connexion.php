f518af34574734f4bdcf7a22e017260ad4eb596d
Hifi58 <gillet.tony@gmail.com>
2021-05-03 08:28:32 +0200

    I'm commit home

--- connexion.php
+++ connexion.php
@@ -1,6 +1,6 @@
 <?php
 try{
-$bdd = new PDO('mysql:host=localhost;dbname=ampoules;charset=utf8', 'root', '');
+$bdd = new PDO('mysql:host=localhost;dbname=ampoules;charset=utf8', 'root', 'root');
 }
 catch (Exception $e)
 {
